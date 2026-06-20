69fd815702eead0840603ef2c5914a3f386e8e2c
ddao19 <dung.dao19@outlook.com>
2026-06-20 10:41:58 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Onboarding from "./pages/Onboarding";
+import Profile from "./pages/Profile";
+import Auth from "./pages/Auth";
+import Account from "./pages/Account";
+import Navbar from "./components/layout/Navbar";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-1">
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/onboarding" element={<Onboarding />} />
+            <Route path="/Profile" element={<Profile />} />
+            <Route path="/auth/:pathname" element={<Auth />} />
+            <Route path="/account/:pathname" element={<Account />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="font-goth">Homepage. TrainFlow AI</h1>
+      <p className="font-inter">This is my TrainFlow project</p>
+    </div>
+  );
+}
